4dea1d74bd15049167b4f17f429999f3035efd0e
top802 <tatrun89@gmail.com>
2022-09-15 19:31:55 +0300

    add

--- src/top/leetcode/Main.java
+++ src/top/leetcode/Main.java
@@ -1,6 +1,7 @@
 package top.leetcode;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 import top.algorithms.MergeTwoArrays;
 import top.algorithms.SelectionSort;
 
@@ -8,12 +9,20 @@ public class Main {
 
   public static void main(String[] args) {
     // write your code here
-    int[] firstArray = {9,3,1,0,6,4};
-    int[] secondArray = {8,7,5,2,11,23};
-    printArray(firstArray);
-    printArray(secondArray);
-    int[] result = MergeTwoArrays.mergeArrays(firstArray, secondArray);
-    printArray(result);
+//    int[] firstArray = {9,3,1,0,6,4};
+//    int[] secondArray = {8,7,5,2,11,23};
+//    printArray(firstArray);
+//    printArray(secondArray);
+//    int[] result = MergeTwoArrays.mergeArrays(firstArray, secondArray);
+//    printArray(result);
+    /**
+     * for n = 10 Fibonacci sequence
+     * 0 1 1 2 3 5 8 13 21 34*/
+//    Stream.iterate(new int[]{0,1},
+//        t -> new int[]{t[1], t[0]+t[1]})
+//        .limit(10)
+//        .map(t -> t[0])
+//        .forEach(System.out::print);
   }
 
   static void printArray(int[] nums){
